97bbc60b42bd73ce6de4368faf7d6e8c04d708bf
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2026-07-27 16:39:58 +0200

    feat: configure user agent

---
+++ internal/version/version.go
@@ -0,0 +1,6 @@
+// Package version holds the build version of the exporter.
+package version
+
+// Version is the current version of the exporter. It is updated by
+// releaser-pleaser on release, do not edit it manually.
+const Version = "0.9.0" // x-releaser-pleaser-version
